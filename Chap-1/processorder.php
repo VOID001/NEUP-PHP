108edeecd0328c2b2d9b3b9abe612eabcd66a2f2
VOID_133 <zhangjianqiu_133@yeah.net>
2014-06-27 08:25:34 +0800

    Update processorder.php

--- Chap-1/processorder.php
+++ Chap-1/processorder.php
@@ -9,14 +9,51 @@
 		$tireqty=$_POST['tireqty'];
 		$oilqty=$_POST['oilqty'];
 		$sparkqty=$_POST['sparkqty'];
-		echo "<p>Order processed at ";
-		echo date('H:i,jS F Y');
-		echo "</p>";
-		echo "<p>Your orders are as follows:</p>";
-		echo "Tire: "  . $tireqty."<br>";
-		echo "Oil: " . $oilqty."<br>";
-		echo "Spark: " .$sparkqty."<br>";
-
+		$find=$_POST['find'];
+		$sq=&$sparkqty;
+		$oq=&$oilqty;
+		$tq=&$tireqty;
+		$totalnum=$sq+$oq+$tq;
+		define('TIREPRICE',50);
+		define('OILPRICE',24);
+		define('SPARKPRICE',60);
+		$totalcost=1.0*($sq*SPARKPRICE+$oq*OILPRICE+$tq*TIREPRICE);
+		$taxrate=0.1;
+		if(!(is_numeric($tq) and is_numeric($oq) and is_numeric($sq)))
+		{
+			echo"<p style='color:red'> <h1>Wrong Requset!</h1></p>";
+		}
+		else if($totalnum<=0) 
+			echo"<p style='color:red'> <h1>You didn't order anything!</h1></p>";
+		else
+		{
+			echo "<p>Order processed at ";
+			echo date('H:i,jS F Y')."<br>";								//date ÈÕÆÚº¯Êý 
+			echo "<h3>Number of Item(s): ".$totalnum."</h3>";	
+			echo "<h3>Total cost of money: $".(double)$totalcost."</h3>";	
+			echo "<h3>Total cost of money: $".number_format((double)$totalcost,3)."</h3>";		//number_format ¸ñÊ½»¯Êä³öº¯Êý
+			$totalcost*=(1+$taxrate);
+			echo "<h3>Total cost of money(Including Tax): ".$totalcost."</h3>";		
+			switch($find)
+			{
+			case "a":
+				echo "<h4>Regular Customer</h4>";
+				break;
+			case "b":
+				echo "<h4>Customer referred by TV advertisement</h4>";
+				break;
+			case "c":
+				echo "<h4>Customer refferred by word of mouth</h4>";
+				break;
+			case "d":
+				echo "<h4>Regular Customer</h4>";
+				break;
+			default:
+				echo"<h4>We do not know how this customer found us</h4>";
+				break;
+			}
+		}
+				
 	?>
 </body>
 </html>
