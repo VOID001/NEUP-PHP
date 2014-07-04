4c904ac1e717007fa25f8691c706aa9f70ec4863
VOID001 <zhangjianqiu_133@yeah.net>
2014-07-04 13:39:29 +0800

    Updated

---
+++ 1/processfeedback.php
@@ -0,0 +1,30 @@
+<?php
+	
+	//create short varaible names
+	$name=$_POST['varname'] ;
+	$email=$_PSOT['varaddress'];
+	$feedback=$_POST['varfeed'];
+
+	$toaddress="zhangjianqiu_133@yeah.net";
+	$subject="Feedback From WebSite";
+	$mailcontent="Customer name=$name\n
+		Customer Email=$email\n
+		Customer comments=\n $feedback\n";
+	$fromaddress="From: webserver@example.com";
+
+	mail($toaddress,$subject,$mailcontent,$fromaddress);
+?>
+
+<html>
+<head>
+	<title>Bob's Auto Parts</title>
+</head>
+<body>
+	<h1>Feedback Submiited</h1>
+	<p>Your feedback has been sent</h1>
+	<br/ >
+	<?php
+	echo "<font color=\"blue\">$feedback</font>";
+	?>
+</body>
+</html>
--- 1/vieworders2.php
+++ 1/vieworders2.php
@@ -1,5 +1,5 @@
 <?php
-	$DOCUMENT_ROOT=$SERVER['DOCUMENT_ROOT'];
+	$DOCUMENT_ROOT=$_SERVER['DOCUMENT_ROOT'];		//这里要注意 不要把 $_SERVER 输入成 $SERVER !!!!!!
 ?>
 <html>
 <head>
@@ -16,6 +16,7 @@
 	//Each order becomes an element in the array 
 	$orders=file("$DOCUMENT_ROOT/1/orders/order.txt");
 	$number_of_orders=count($orders);
+	echo "$DOCUMENT_ROOT/1/orders/order.txt";
 	if ($number_of_orders==0)
 	{
 		echo "<p><strong>No Orders pending .
@@ -31,14 +32,14 @@
 		echo "<th bgcolor =\"#CCCCFF\">Total</th>";
 		echo "<th bgcolor =\"#CCCCFF\">Address</th>";
 		echo "</tr>";
-		for($i=0;i<$number_of_orders;$i++)
+		echo "<h4>$number_of_orders</h4>";
+		for($i=0;$i<$number_of_orders;$i++)
 		{
-			$line = explode("\t",$orders[i]);
+			$line = explode("\t",$orders[$i]);				//一定要注意 PHP的变量名前要加 $ 号！！！！
 
 			$line[1]=intval($line[1]);
 			$line[2]=intval($line[2]);
 			$line[3]=intval($line[3]);
-
 			echo"<tr>
 				<td >$line[0]</td>
 				<td align= \"right\">$line[1]</td>
@@ -47,9 +48,11 @@
 				<td align= \"right\">$line[4]</td>
 				<td>$line[5]</td>
 				</tr>";
+			echo "<h4>$orders[$i]</h4>";
 		}
 		echo "</table>";
 	}
 ?>
 
 </body>
+</html>
